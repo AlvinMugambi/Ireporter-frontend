9048485c3a18e6f4a191a28675c17d914507c05e
Alvin Mugambi <alvin@Alvins-MacBook-Pro.local>
2019-04-16 15:26:54 +0300

    add heroku configurations

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './component/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '/dist'),
     filename: 'index_bundle.js',
     publicPath: '/'
   },
